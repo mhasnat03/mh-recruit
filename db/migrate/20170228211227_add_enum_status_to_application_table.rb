e99974a3d5f6f68e3d6a0e173504fde03649ecc7
Muntasir Hasnat <mhasnat@greenhouse.io>
2017-03-01 20:53:34 -0500

    Fixed the failing migration.

--- db/migrate/20170228211227_add_enum_status_to_application_table.rb
+++ db/migrate/20170228211227_add_enum_status_to_application_table.rb
@@ -1,7 +1,5 @@
 class AddEnumStatusToApplicationTable < ActiveRecord::Migration
   def change
-  		remove_column(:applications, :status)
-  		add_column(:applications, :status, :integer, default: 2)
-  		
+  		add_column(:applications, :status, :integer, default: 2)  		
   end
 end
